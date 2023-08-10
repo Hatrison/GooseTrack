95610409aefe18b5f11816f2662d5d618bb38322
Інга Юрченко <missingusya@hotmail.com>
2023-08-10 21:16:22 +0300

    updated selectors.js

--- src/redux/auth/selectors.js
+++ src/redux/auth/selectors.js
@@ -1,5 +1,7 @@
 export const selectIsLoggedIn = state => state.auth.isLoggedIn;
 
+export const selectError = state => state.auth.error;
+
 export const selectIsRefreshing = state => state.auth.isRefreshing;
 
 export const selectToken = state => state.auth.token;
