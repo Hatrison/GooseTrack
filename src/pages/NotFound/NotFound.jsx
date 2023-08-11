8f5f303f5aa433ef1bcfaccbd5b5aafb4263f995
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-08-11 10:35:00 +0300

    add styles

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,5 +1,31 @@
+// import { Link } from 'react-router-dom';
+import {
+  Text,
+  Image,
+  NumberWrap,
+  Number,
+  Wtapper,
+  StyledLink,
+  ContentWrap,
+} from './NotFound.styled';
+
 const NotFound = () => {
-  return <h1>Not Found</h1>;
+  return (
+    <Wtapper>
+      <ContentWrap>
+        <NumberWrap>
+          <Number>4</Number>
+          <Image src="" alt="Rocket" width="81px" height="188px" />
+          <Number>4</Number>
+        </NumberWrap>
+        <Text>
+          We’re sorry, the page you requested could not be found. Please go back
+          to the homepage.
+        </Text>
+        <StyledLink>Back to home</StyledLink>
+      </ContentWrap>
+    </Wtapper>
+  );
 };
 
 export default NotFound;
---
+++ src/pages/NotFound/NotFound.styled.js
@@ -0,0 +1,88 @@
+import styled from 'styled-components';
+import { Link } from 'react-router-dom';
+
+export const Wtapper = styled.div`
+  position: relative;
+  max-width: 100%;
+  height: 100vh;
+  background-color: ${({ theme }) => theme.secondaryBackgroundColor};
+`;
+
+export const ContentWrap = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  max-width: 281px;
+  background-color: ${({ theme }) => theme.secondaryBackgroundColor};
+
+  @media screen and (min-width: 768px) {
+    max-width: 503px;
+  }
+`;
+export const NumberWrap = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 28px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 80px;
+  }
+`;
+
+export const Number = styled.p`
+  font-size: 100px;
+  line-height: 1.5;
+  font-weight: bold;
+  color: ${({ theme }) => theme.accentColor};
+
+  @media screen and (min-width: 768px) {
+    font-size: 200px;
+    line-height: 1.25;
+  }
+`;
+
+export const Image = styled.img`
+  display: block;
+  max-width: 100%;
+  height: auto;
+  transform: rotate(-31deg);
+  outline: 1px dashed red;
+`;
+export const Text = styled.p`
+  line-height: 1.29;
+  text-align: center;
+  font-weight: 500;
+  color: ${({ theme }) => theme.mainTextColor};
+  opacity: 0.7;
+  margin-bottom: 24px;
+
+  @media screen and (min-width: 768px) {
+    margin-bottom: 32px;
+  }
+`;
+
+export const StyledLink = styled(Link)`
+  display: block;
+  width: max-content;
+  font-size: 14px;
+  line-height: 1.29;
+  letter-spacing: -2%;
+  font-weight: 600;
+  margin: 0 auto;
+  padding: 14px 32px;
+  border-radius: 16px;
+  background-color: ${({ theme }) => theme.accentColor};
+  color: ${({ theme }) => theme.buttontextColor};
+
+  &:hover {
+    background-color: ${({ theme }) => theme.buttonBackgroundColorHover};
+  }
+
+  @media screen and (min-width: 768px) {
+    padding: 16px 48px;
+    font-size: 18px;
+    line-height: 1.33;
+  }
+`;
