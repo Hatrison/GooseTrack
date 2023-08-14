8d625bd618650f0ee54ab26aff32f8457be96018
Stepan Skichko <st.skichko@gmail.com>
2023-08-14 13:06:17 +0300

    add taskcolumncard render logic

--- src/components/ChoosedDay/TasksColumnsList/TasksColumn/components/TaskColumnCard/TaskColumnCard.jsx
+++ src/components/ChoosedDay/TasksColumnsList/TasksColumn/components/TaskColumnCard/TaskColumnCard.jsx
@@ -6,11 +6,13 @@ import {
   Wrapper,
   Avatar,
   PriorityLow,
+  PriorityHigh,
+  PriorityMedium,
 } from './TaskColumnCard.styled';
 import { TaskToolbar } from '../index';
 import { selectUser } from 'redux/user/selectors';
 
-export const TaskColumnCard = () => {
+export const TaskColumnCard = ({ items }) => {
   const { avatarURL, name } = useSelector(selectUser);
 
   return (
@@ -27,5 +29,21 @@ export const TaskColumnCard = () => {
         <TaskToolbar />
       </ToolbarBox>
     </TaskBox>
+    //  <>
+    //       {items?.map(element => (
+    //        <TaskBox key={element.id}>
+    //          <TaskText>{element.text}</TaskText>
+    //          <ToolbarBox>
+    //            <Wrapper>
+    //              <Avatar src={avatarURL} alt={name} />
+    //              {element.priority === 'high' && <PriorityHigh>{element.priority}</PriorityHigh>}
+    //              {element.priority === 'medium' && <PriorityMedium>{element.priority}</PriorityMedium>}
+    //              {element.priority === 'low' && <PriorityLow>{element.priority}</PriorityLow>}
+    //            </Wrapper>
+    //            <TaskToolbar/>
+    //          </ToolbarBox>
+    //        </TaskBox>
+    //      )}
+    //    </>
   );
 };
