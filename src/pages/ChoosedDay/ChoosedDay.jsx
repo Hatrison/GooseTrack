6b8781dfd5dea57fc5a59e0e9e4ff48a772dd532
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-14 23:16:48 +0300

    Deleted unnecessary text

--- src/pages/ChoosedDay/ChoosedDay.jsx
+++ src/pages/ChoosedDay/ChoosedDay.jsx
@@ -1,12 +1,8 @@
-import { Link } from 'react-router-dom';
 import { DayCalendarHead, TasksColumnsList } from 'components/ChoosedDay';
 
 const ChoosedDay = ({ propSelectedWeek }) => {
   return (
     <div>
-      ChoosedDay
-      <br />
-      <Link to="/calendar/month/:currentDate">Month</Link>
       <DayCalendarHead selectedWeek={propSelectedWeek} />
       <TasksColumnsList />
     </div>
