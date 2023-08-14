169721088caab8383ab2bb031409560783be5c16
Stepan Skichko <st.skichko@gmail.com>
2023-08-14 10:13:14 +0300

    add the same logic to ColumnHeadBar

--- src/components/ChoosedDay/TasksColumnsList/TasksColumn/components/ColumnHeadBar/ColumnHeadBar.jsx
+++ src/components/ChoosedDay/TasksColumnsList/TasksColumn/components/ColumnHeadBar/ColumnHeadBar.jsx
@@ -1,13 +1,23 @@
+// import { useState } from 'react';
 import { BtnAdd, PlusCircleIcon, Box, Title } from './ColumnHeadBar.styled';
 
 export const ColumnHeadBar = ({ title }) => {
+  // const [isTaskModalOpened, setIsTaskModalOpened] = useState(false);
+
+  // const handleToggle = () => setIsTaskModalOpened(prevState => !prevState);
+
   return (
     <Box>
       {/* <Title>{title}</Title> */}
       <Title>To Do</Title>
-      <BtnAdd>
+      <BtnAdd
+      // onClick={handleToggle}
+      >
         <PlusCircleIcon />
       </BtnAdd>
+      {/* {isTaskModalOpened &&
+        <TaskModal onClose={handleToggle}></TaskModal>
+      }, */}
     </Box>
   );
 };
