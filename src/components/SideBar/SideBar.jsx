b62a9e8a7a82ff47358ab353b685d7e846324fee
ishtvanb <110969832+ishtvanb@users.noreply.github.com>
2023-08-14 16:29:58 +0300

    Finished Sidebar

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,62 +1,91 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { useMediaQuery } from 'react-responsive';
 import { logoutUser } from 'redux/auth/operations';
+import { LogOutBtn } from './SideBar.styled';
+import { ReactComponent as ButtonIcon } from 'images/svg/logout.svg';
+import { ReactComponent as CloseIcon} from 'images/svg/close.svg';
+import { ReactComponent as Logo } from 'images/svg/logo_text.svg';
+import { ReactComponent as User } from 'images/svg/user.svg';
+import { ReactComponent as Calendar } from 'images/svg/calendarCheck.svg';
+import { ReactComponent as Chart } from 'images/svg/chart.svg';
 
 import {
   SidebarOverlay,
   SidebarWrap,
+  SidebarCover,
   Link,
-  Navigation,
+  NavLink,
 } from './SideBar.styled';
 
 export default function SideBar({ onSidebarToggle }) {
+  const [closeIconVisible, setCloseIconVisible] = useState(false);
   const dispatch = useDispatch();
   const isDesktop = useMediaQuery({ query: '(min-width: 1440px)' });
-  useEffect(() => {
-    const closeMenuByEsc = e => {
-      if (e.code === 'Escape') {
+  if (isDesktop || !closeIconVisible) {
+      document.body.style.show = 'false';
+    } else {
+      document.body.style.show = 'true';
+  }
+  
+    const showCloseIcon = () => {
+      setCloseIconVisible(state => !state);
+    }
+    
+    useEffect(() => {
+      const closeMenuByEsc = e => {
+        if (e.code === 'Escape') {
+          onSidebarToggle();
+        }
+      };
+
+      document.addEventListener('keydown', closeMenuByEsc);
+
+      return () => {
+        document.removeEventListener('keydown', closeMenuByEsc);
+      };
+    }, [onSidebarToggle]);
+
+    const onOverlayClick = event => {
+      if (event.target === event.currentTarget) {
         onSidebarToggle();
       }
     };
 
-    document.addEventListener('keydown', closeMenuByEsc);
-
-    return () => {
-      document.removeEventListener('keydown', closeMenuByEsc);
+    const handleLogout = () => {
+      dispatch(logoutUser());
+      !isDesktop && onSidebarToggle();
     };
-  }, [onSidebarToggle]);
-
-  const onOverlayClick = event => {
-    if (event.target === event.currentTarget) {
-      onSidebarToggle();
-    }
+  
+    return (
+      <SidebarOverlay onClick={onOverlayClick}>
+        <SidebarWrap>
+          <div>
+            <SidebarCover>
+              <div>
+                <Logo />
+                {(!isDesktop || closeIconVisible) && (
+                  <CloseIcon showCloseIcon={showCloseIcon}/>
+                )}
+              </div>
+            </SidebarCover>
+            <LogOutBtn type='button'>
+              Log out
+              <ButtonIcon />
+            </LogOutBtn>
+          </div>
+          <div>
+            <p>User Panel</p>
+            <NavLink>
+              <Link to="/account"><User />My account</Link>
+              <Link to="/calendar"><Calendar />Calendar</Link>
+              <Link to="/statistics"><Chart />Statistics</Link>
+            </NavLink>
+          </div>
+          <button type="button" onClick={handleLogout}>
+            Log out
+          </button>
+        </SidebarWrap>
+      </SidebarOverlay>
+    );
   };
-
-  const handleLogout = () => {
-    dispatch(logoutUser());
-    !isDesktop && onSidebarToggle();
-  };
-  return (
-    <SidebarOverlay onClick={onOverlayClick}>
-      <SidebarWrap>
-        <div>
-          <div>logo</div>
-          <div>WordLogo</div>
-          <div>Button</div>
-        </div>
-        <div>
-          <p>User Panel</p>
-          <Navigation>
-            <Link to="/account">My account</Link>
-            <Link to="/calendar">Calendar</Link>
-            <Link to="/statistics">Statistics</Link>
-          </Navigation>
-        </div>
-        <button type="button" onClick={handleLogout}>
-          Log out
-        </button>
-      </SidebarWrap>
-    </SidebarOverlay>
-  );
-}
--- src/components/SideBar/SideBar.styled.jsx
+++ src/components/SideBar/SideBar.styled.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { Link as SidebarLink } from 'react-router-dom';
+
 export const SidebarOverlay = styled.div`
   @media (max-width: 1439px) {
     z-index: 2;
@@ -12,8 +12,8 @@ export const SidebarOverlay = styled.div`
 `;
 
 export const SidebarWrap = styled.div`
-  border: 1px solid blue;
-  background-color: white;
+  border: 1px solid #3E85F3;
+  background-color: #ffffff;
   min-height: 100vh;
   width: 290px;
   padding-left: 24px;
@@ -40,10 +40,219 @@ export const SidebarWrap = styled.div`
   }
 `;
 
-export const Navigation = styled.nav`
+export const SidebarCover = styled.div`
   display: flex;
-  flex-direction: column;
-  gap: 20px;
+  flex-direction: row;
 `;
 
-export const Link = styled(SidebarLink)``;
+export const NavLink = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 18px;
+
+  @media (min-width: 768px),
+  @media (min-width: 1440px) {
+  gap: 16px;
+}
+`;
+
+export const ButtonIcon = styled.svg`
+  width: 18px;
+  height: 18px;
+  margin-left: 6px;
+  stroke: #ffffff;
+
+  @media (min-width: 768px),
+  @media (min-width: 1440px) {
+  width: 20px;
+  height: 20px;
+  margin-left: 11px;
+}
+`;
+
+export const User = styled.svg`
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
+  stroke: #000000;
+
+  @media (min-width: 768px),
+  @media (min-width: 1440px) {
+  width: 24px;
+  height: 24px;
+  margin-right: 11px;
+}
+
+&:hover {
+    {background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+    (stroke: #3E85F3) : (stroke: #ffffff)};
+  }
+
+{background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+(stroke: #000000) : (stroke: #ffffff)};
+
+{{background-color: ${({ theme }) => !theme.secondaryBackgroundColor}} && {isActive}
+ ? 
+(stroke: #3E85F3) : (stroke: #000000)};
+`;
+
+export const Calendar = styled.svg`
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
+  stroke: #000000;
+
+  @media (min-width: 768px),
+  @media (min-width: 1440px) {
+  width: 24px;
+  height: 24px;
+  margin-right: 11px;
+}
+
+&:hover {
+    {background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+    (stroke: #3E85F3) : (stroke: #ffffff)};
+  }
+
+{background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+(stroke: #000000) : (stroke: #ffffff)};
+
+{{background-color: ${({ theme }) => !theme.secondaryBackgroundColor}} && {isActive}
+ ? 
+(stroke: #3E85F3) : (stroke: #000000)};
+`;
+
+export const Chart = styled.svg`
+  width: 20px;
+  height: 20px;
+  margin-right: 8px;
+  stroke: #000000;
+
+  @media (min-width: 768px),
+  @media (min-width: 1440px) {
+  width: 24px;
+  height: 24px;
+  margin-right: 11px;
+}
+
+&:hover {
+    {background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+    (stroke: #3E85F3) : (stroke: #ffffff)};
+  }
+
+{background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+(stroke: #000000) : (stroke: #ffffff)};
+
+{{background-color: ${({ theme }) => !theme.secondaryBackgroundColor}} && {isActive}
+ ? 
+(stroke: #3E85F3) : (stroke: #000000)};
+`;
+
+export const LogOutBtn = styled.button`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  
+  width: 131px;
+  height: 46px;
+  border: 0;
+  border-radius: 16px;
+  background-color: #3E85F3;
+  color: #FFFFFF;
+  cursor: pointer;
+
+  font-family: 'InterSemiBolt';
+  font-style: normal;
+  font-weight: 600;
+  line-height: 1.3;
+  font-size: 24px;
+
+  padding: 28px 14px;
+  margin-top: 20px 424px 74px 24px;
+
+  &:hover ${ButtonIcon} {
+ 
+  animation: scale-up-hor-left 0.7s cubic-bezier(0.39, 0.575, 0.565, 1) both;
+}
+
+@media (min-width: 768px) {
+  width: 141px;
+  height: 56px;
+  margin: 32px 563px 116px 24px;
+  padding: 23px 16px;
+}
+
+@media (min-width: 1440px) {
+  width: 141px;
+  height: 56px;
+  margin: 24px 439px 124px 24px;
+  padding: 23px 16px;
+}
+`;
+
+export const CloseIcon = styled.svg`
+  width: 24px;
+  height: 24px;
+  margin-left: 25px;
+  border: 0;
+  stroke: #000000;
+  fill: transparent;
+
+  @media (min-width: 768px) {
+    width: 34px;
+    height: 34px;
+    margin-left: 31px;
+}
+
+{background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+(stroke: #000000) : (stroke: #ffffff)};
+`;
+
+export const Logo = styled.logo`
+  margin-bottom: 64px;
+
+@media (min-width: 768px) {
+  margin-bottom: 50px;
+}
+
+@media (min-width: 1440px) {
+  margin-bottom: 32px;
+}
+
+{background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+(fill: #3E85F3) : (fill: #FFFFFF)};
+`;
+
+export const Link = styled.link`
+  display: inline-block;
+  width: 185px;
+  height: 40px;
+  font-size: 14px;
+  font-weight: 600;
+  border-radius: 8px;
+  padding: 12px, 10px, 65px, 10px;
+  background-color: transparent;
+
+  @media (min-width: 768px) {
+  width: 225px;
+  height: 56px;
+  padding: 20px, 16px, 80px, 16px;
+  font-size: 16px;
+}
+
+  @media (min-width: 1440px) {
+  width: 241px;
+  height: 56px;
+  padding: 20px, 16px, 96px, 16px;
+  font-size: 16px;
+}
+
+{background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+(color: #000000) : (color: #FFFFFF)};
+
+&:hover {
+    {background-color: ${({ theme }) => !theme.secondaryBackgroundColor} ? 
+    (background-color: #E3F3FF) : (background-color: #3E85F3)};
+
+  }
+`;
