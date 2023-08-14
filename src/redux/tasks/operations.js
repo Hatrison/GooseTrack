d5eb4926afbe30e81f22b652ad9d6d8f252d75e7
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-14 22:16:09 +0300

    Added operations for task slice

--- src/redux/tasks/operations.js
+++ src/redux/tasks/operations.js
@@ -4,14 +4,52 @@ import axios from 'axios';
 const { REACT_APP_BASE_URL } = process.env;
 axios.defaults.baseURL = `${REACT_APP_BASE_URL}`;
 
-export const fetchTasks = createAsyncThunk(
-  'tasks/fetchMonth',
+export const getTasks = createAsyncThunk(
+  'getTasks',
   async ({ year, month }, thunkAPI) => {
     try {
-      const response = await axios.get(`/tasks/?year=${year}&month=${month}`);
+      const response = await axios.get(
+        `/api/tasks/?year=${year}&month=${month}`
+      );
       return response.data;
-    } catch (e) {
-      return thunkAPI.rejectWithValue(e.message);
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const addTask = createAsyncThunk(
+  'createTask',
+  async (task, thunkAPI) => {
+    try {
+      const response = await axios.post(`/api/tasks`, task);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const updateTask = createAsyncThunk(
+  'updateTask',
+  async (task, thunkAPI) => {
+    try {
+      const response = await axios.patch(`/api/tasks/${task._id}`, task);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const deleteTask = createAsyncThunk(
+  'deleteTask',
+  async (id, thunkAPI) => {
+    try {
+      const response = await axios.delete(`/api/tasks/${id}`);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
   }
 );
