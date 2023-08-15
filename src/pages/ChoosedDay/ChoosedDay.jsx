a7b1fbc462f3de15f5d9e5366c6e09ff86461c3f
Stepan Skichko <st.skichko@gmail.com>
2023-08-15 11:01:10 +0300

    update remarks

--- src/pages/ChoosedDay/ChoosedDay.jsx
+++ src/pages/ChoosedDay/ChoosedDay.jsx
@@ -1,15 +1,6 @@
-// import { useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
-// import { fetchTasks } from '../../redux/tasks/operations';
 import { DayCalendarHead, TasksColumnsList } from 'components/ChoosedDay';
 
 const ChoosedDay = ({ propSelectedWeek }) => {
-  // const dispatch = useDispatch();
-
-  // useEffect(() => {
-  //   dispatch(fetchTasks());
-  // }, [dispatch]);
-
   return (
     <div>
       <DayCalendarHead selectedWeek={propSelectedWeek} />
