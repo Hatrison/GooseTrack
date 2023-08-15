d2553aa84711fc71bdbdf0eb874dca06ab7e7152
NataliTs <natikz20@ukr.net>
2023-08-15 09:21:56 +0300

    murkup

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -1,14 +1,28 @@
+import React, { useState } from 'react';
 import { Formik, Field, ErrorMessage } from 'formik';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 import moment from 'moment/moment';
 import { UserValidSchema } from './UserValidSchema';
-import { Form, UserInfo, UserWrapper } from './UserForm.styled';
+import {
+  AddIcon,
+  AvatarWrap,
+  DefaultAvatar,
+  FieldAdd,
+  Form,
+  UserInfo,
+  UserWrapper,
+  Wrapper,
+  WrapperForm,
+} from './UserForm.styled';
+import defaultAvatar from '../../images/avatar.png';
 
 const currentDate = moment().format('DD/MM/YYYY');
 
 const UserForm = () => {
+  const [avatarURL, setAvatarURL] = useState(null);
+
   return (
     <Formik
       validationSchema={UserValidSchema}
@@ -24,62 +38,92 @@ const UserForm = () => {
       }}
     >
       <Form>
-        <UserWrapper>
-          <UserInfo>
-            <label>
-              User Name
-              <Field name="name" />
-              <ErrorMessage name="name" component="span" />
-            </label>
-
-            <label>
-              Birthday
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  name="birthday"
-                  type="date"
-                  slotProps={{
-                    textField: {
-                      // placeholder: birthdayNumber || `${currentDate}`,
-                      placeholder: `${currentDate}`,
-                    },
-                  }}
-                  views={['year', 'month', 'day']}
-                  format="DD/MM/YYYY"
-                  closeOnSelect={true}
-                  disableFuture={true}
-                />
-              </LocalizationProvider>
-            </label>
+        <WrapperForm>
+          <AvatarWrap>
             <div>
-              <label>
-                Email
-                <Field name="email" type="email" />
-                <ErrorMessage name="email" component="span" />
-              </label>
+              {/* <label htmlFor="avatar">
+                <img src={URL.createObjectURL(avatarURL)} alt="avatar" />
+              </label> */}
+              {avatarURL ? (
+                <label htmlFor="avatar">
+                  <img src={URL.createObjectURL(avatarURL)} alt="Avatar" />
+                </label>
+              ) : (
+                <DefaultAvatar>
+                  <img src={defaultAvatar} alt="Avatar" />
+                </DefaultAvatar>
+              )}
             </div>
-          </UserInfo>
-
-          <UserInfo>
-            <label>
-              Phone
-              <Field name="phone" type="tel" placeholder="+380XXXXXXXXX" />
-              <ErrorMessage name="phone" component="span" />
+            <FieldAdd
+              id="add-avatar"
+              name="avatar"
+              type="file"
+              accept="image/*,.png,.jpg,.gif,.web"
+              onChange={e => {
+                const file = e.target.files[0];
+                setAvatarURL(file);
+              }}
+            />
+            <label htmlFor="add-avatar">
+              <AddIcon />
             </label>
+          </AvatarWrap>
+          <UserWrapper>
+            <UserInfo>
+              <label>
+                User Name
+                <Field name="name" />
+                <ErrorMessage name="name" component="span" />
+              </label>
 
-            <label>
-              Skype
-              <Field
-                name="skype"
-                type="text"
-                placeholder="Add a skype number"
-              />
-              <ErrorMessage name="skype" component="span" />
-            </label>
-          </UserInfo>
-        </UserWrapper>
+              <label>
+                Birthday
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    name="birthday"
+                    type="date"
+                    slotProps={{
+                      textField: {
+                        placeholder: `${currentDate}`,
+                      },
+                    }}
+                    views={['year', 'month', 'day']}
+                    format="DD/MM/YYYY"
+                    closeOnSelect={true}
+                    disableFuture={true}
+                  />
+                </LocalizationProvider>
+              </label>
+              <div>
+                <label>
+                  Email
+                  <Field name="email" type="email" />
+                  <ErrorMessage name="email" component="span" />
+                </label>
+              </div>
+            </UserInfo>
 
-        <button type="submit">Save changes</button>
+            <UserInfo>
+              <label>
+                Phone
+                <Field name="phone" type="tel" placeholder="+380XXXXXXXXX" />
+                <ErrorMessage name="phone" component="span" />
+              </label>
+
+              <label>
+                Skype
+                <Field
+                  name="skype"
+                  type="text"
+                  placeholder="Add a skype number"
+                />
+                <ErrorMessage name="skype" component="span" />
+              </label>
+            </UserInfo>
+          </UserWrapper>
+
+          <button type="submit">Save changes</button>
+        </WrapperForm>
       </Form>
     </Formik>
   );
--- src/components/UserForm/UserForm.styled.jsx
+++ src/components/UserForm/UserForm.styled.jsx
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
-import { Form as FormikForm } from 'formik';
+import { Form as FormikForm, Field as FormikFieldAdd } from 'formik';
+import { ReactComponent as plusCircle } from '../../images/svg/plus.svg';
+import { ReactComponent as Avatar } from '../../images/svg/user.svg';
 
 export const Form = styled(FormikForm)``;
+
+export const FieldAdd = styled(FormikFieldAdd)`
+  display: none;
+`;
 
 export const UserWrapper = styled.div`
   display: flex;
@@ -9,8 +15,35 @@ export const UserWrapper = styled.div`
   gap: 50px;
 `;
 
+export const WrapperForm = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 50px;
+`;
+
+export const AvatarWrap = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
 export const UserInfo = styled.div`
   display: flex;
   flex-direction: column;
   gap: 30px;
+`;
+
+export const AddIcon = styled(plusCircle)`
+  width: 24px;
+  height: 24px;
+  display: block;
+  background-color: blue;
+`;
+
+export const DefaultAvatar = styled.div`
+  width: 100px;
+  height: 100px;
+  display: block;
+  background-color: blue;
 `;
