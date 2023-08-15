670d357f3d078fdfe7bc3579a37769528b7d52a7
AnastasiiaYurchenko <nastya.brok@ukr.net>
2023-08-16 01:13:08 +0300

    add function getAllReviews

--- src/components/ReviewsSlider/ReviewsSlider.jsx
+++ src/components/ReviewsSlider/ReviewsSlider.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+// import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import {
   Section,
   Container,
@@ -16,13 +18,84 @@ import {
   StarsList,
 } from './ReviewsSlider.styled';
 
+export const getAllReviews = async () => {
+  const response = await axios.get(
+    'https://goosetrack-0ajq.onrender.com/api/reviews'
+  );
+  if (response.status === 404) {
+    throw new Error('Something went wrong, please try again', response.status);
+  }
+  return response.data;
+};
+
 const ReviewsSlider = () => {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    async function getReviews() {
+      try {
+        // setLoading(true);
+        // setError(null);
+        const allReviews = await getAllReviews();
+        console.log(allReviews);
+
+        setReviews(allReviews);
+        // setMovies(trendingMovies.results);
+      } catch (error) {
+        // setError(ERROR_MSG);
+        console.log(error);
+      } finally {
+        // setLoading(false);
+      }
+    }
+    getReviews();
+  }, []);
+
   return (
     <Section>
       <Container>
         <Title>Reviews</Title>
         <List>
-          <Item>
+          {reviews &&
+            reviews.map(review => {
+              return (
+                <Item key={review._id}>
+                  <WrapAvatarNameStars>
+                    <Avatar
+                      src="https://366icons.com/media/01/profile-avatar-account-icon-16699.png"
+                      alt="Avatar"
+                      width="50px"
+                      height="50px"
+                    />
+                    <div>
+                      <Name>
+                        {review.name} - {review.rating}
+                      </Name>
+                      <StarsList>
+                        <li>
+                          <RatingStar />
+                        </li>
+                        <li>
+                          <RatingStar />
+                        </li>
+                        <li>
+                          <RatingStar />
+                        </li>
+                        <li>
+                          <RatingStar />
+                        </li>
+                        <li>
+                          <RatingStar />
+                        </li>
+                      </StarsList>
+                    </div>
+                  </WrapAvatarNameStars>
+
+                  <Text>{review.text}</Text>
+                </Item>
+              );
+            })}
+          {/* <Item>
             <WrapAvatarNameStars>
               <Avatar
                 src="../../images/avatar1.jpg"
@@ -92,7 +165,7 @@ const ReviewsSlider = () => {
               GooseTrack is impressive, the calendar view and filter options
               make it easy to stay organized and focused. Highly recommended.
             </Text>
-          </Item>
+          </Item> */}
         </List>
         <ArrowsWrap>
           <ArrowLeft />
