0adedf3cb84f723b7d6dd82279bf26795e6b4bf8
Stepan Skichko <st.skichko@gmail.com>
2023-08-15 13:46:47 +0300

    little update

--- src/components/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
+++ src/components/ChoosedDay/TasksColumnsList/TasksColumnsList.jsx
@@ -1,7 +1,14 @@
 import { List } from './TasksColumnsList.styled';
 import { TasksColumn } from './TasksColumn/TasksColumn';
+// import { selectTasks } from 'redux/tasks/selectors';
 
 export const TasksColumnsList = () => {
+  // const todoTasks = selectTasks.filter(task => task.category === 'to-do');
+  // const inProgressTasks = selectTasks.filter(
+  //   task => task.category === 'in-progress'
+  // );
+  // const doneTasks = selectTasks.filter(task => task.category === 'done');
+
   return (
     <List>
       <TasksColumn />
--- src/pages/ChoosedDay/ChoosedDay.jsx
+++ src/pages/ChoosedDay/ChoosedDay.jsx
@@ -1,6 +1,15 @@
+import { useDispatch } from 'react-redux';
 import { DayCalendarHead, TasksColumnsList } from 'components/ChoosedDay';
+import { selectTasks } from 'redux/tasks/selectors';
+import { useEffect } from 'react';
 
 const ChoosedDay = ({ propSelectedWeek }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(selectTasks);
+  }, [dispatch]);
+
   return (
     <div>
       <DayCalendarHead selectedWeek={propSelectedWeek} />
