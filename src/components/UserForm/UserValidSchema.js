44f129528a1cc9e19b7e591ea8e341bd99b41cb0
NataliTs <natikz20@ukr.net>
2023-08-16 12:02:25 +0300

    phone

--- src/components/UserForm/UserValidSchema.js
+++ src/components/UserForm/UserValidSchema.js
@@ -1,6 +1,7 @@
 import * as Yup from 'yup';
 
 const regexPhone = /^\+380\d{9}$/;
+// const regexPhone = /^\d{2} \(\d{3}\) \d{3} \d{2} \d{2}$/;
 const regexSkype = /^\S[\S\s]{0,28}\S$/;
 
 export const UserValidSchema = () =>
@@ -12,11 +13,12 @@ export const UserValidSchema = () =>
     birthday: Yup.string().notRequired(),
     email: Yup.string().email('Invalid email').required('Email is required'),
     phone: Yup.string()
-      .matches(regexPhone, {
-        message: 'The phone number must start with +380 and be 9 digits long',
-      })
+      .matches(
+        regexPhone,
+        'The phone number must start with +380 and be 9 digits long'
+      )
       .min(13, 'Phone is too Short!')
-      .max(13, 'Phone is too Long!')
+      .max(17, 'Phone is too Long!')
       .notRequired(),
     skype: Yup.string()
       .matches(regexSkype, 'Skype must be between 3 and 16 characters')
