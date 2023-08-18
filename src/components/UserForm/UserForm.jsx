13adab43706d6fe66a187394f217b6aa918de9dc
NataliTs <natikz20@ukr.net>
2023-08-18 19:25:46 +0300

    upd upload avatar, Birthday

--- src/components/UserForm/UserForm.jsx
+++ src/components/UserForm/UserForm.jsx
@@ -5,32 +5,31 @@ import { Formik, Field, ErrorMessage } from 'formik';
 import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import moment from 'moment/moment';
+import dayjs from 'dayjs';
 import { toast } from 'react-toastify';
 import { UserValidSchema } from './UserValidSchema';
 import { updateUser } from '../../redux/user/operations';
 import {
   AddIcon,
-  // AvatarWrap,
   AvatarWrapper,
   DefaultAvatar,
   FieldAdd,
   Form,
-  // ImageAvatar,
   ImgAvatar,
   UserInfo,
   UserWrapper,
   WrapperForm,
 } from './UserForm.styled';
-import defaultAvatar from '../../images/avatar.png';
+import defaultAvatar from '../../images/account/avatar.png';
 
-const currentDate = moment().format('DD/MM/YYYY');
+const currentDate = dayjs(new Date()).format('YYYY/MM/DD');
 
 const UserForm = () => {
   const dispatch = useDispatch();
   const userInfo = useSelector(selectUser);
 
   const [avatarURL, setAvatarURL] = useState(null);
+  const [birthDate, setBirthDate] = useState(null);
 
   const handleSubmit = async values => {
     const formData = new FormData();
@@ -42,7 +41,10 @@ const UserForm = () => {
     if (values.skype) {
       formData.append('skype', values.skype);
     }
-    formData.append('birthday', values.birthday);
+    formData.append(
+      'birthday',
+      birthDate ? dayjs(birthDate).format('YYYY/MM/DD') : ''
+    );
     if (avatarURL) {
       formData.append('avatar', avatarURL);
     }
@@ -55,108 +57,118 @@ const UserForm = () => {
   };
 
   return (
-    <Formik
-      validationSchema={UserValidSchema}
-      initialValues={{
-        name: userInfo?.name || '',
-        birthday: userInfo?.birthday || `${currentDate}`,
-        email: userInfo?.email || '',
-        phone: userInfo?.phone || '',
-        skype: userInfo?.skype || '',
-      }}
-      onSubmit={handleSubmit}
-    >
-      <Form>
-        <WrapperForm>
-          <AvatarWrapper>
-            <ImgAvatar>
-              {avatarURL ? (
-                <label htmlFor="avatar">
-                  <img src={URL.createObjectURL(avatarURL)} alt="Avatar" />
-                </label>
-              ) : userInfo?.avatarURL ? (
-                <label htmlFor="avatar">
-                  <img src={userInfo?.avatarURL} alt="Avatar" />
-                </label>
-              ) : (
-                <DefaultAvatar>
-                  <img src={defaultAvatar} alt="Avatar" />
-                </DefaultAvatar>
-              )}
-            </ImgAvatar>
-            <FieldAdd
-              id="add-avatar"
-              name="avatar"
-              type="file"
-              accept="image/*, .png,.jpg,.gif,.web, .webp"
-              onChange={e => {
-                setAvatarURL(e.target.files[0]);
-              }}
-            />
-            <label htmlFor="add-avatar">
-              <AddIcon />
-            </label>
-          </AvatarWrapper>
-          <h1>{userInfo.name}</h1>
-          <p>User</p>
-          <UserWrapper>
-            <UserInfo>
-              <label>
-                User Name
-                <Field name="name" />
-                <ErrorMessage name="name" component="span" />
+    <WrapperForm>
+      <Formik
+        validationSchema={UserValidSchema}
+        initialValues={{
+          name: userInfo?.name || '',
+          birthday: userInfo?.birthday || `${currentDate}`,
+          email: userInfo?.email || '',
+          phone: userInfo?.phone || '',
+          skype: userInfo?.skype || '',
+        }}
+        onSubmit={handleSubmit}
+      >
+        {({ values, setFieldValue }) => (
+          <Form>
+            <div>
+              <AvatarWrapper>
+                {avatarURL ? (
+                  <label htmlFor="avatar">
+                    <ImgAvatar
+                      src={URL.createObjectURL(avatarURL)}
+                      alt="Avatar"
+                    />
+                  </label>
+                ) : userInfo?.avatarURL ? (
+                  <label htmlFor="avatar">
+                    <ImgAvatar src={userInfo?.avatarURL} alt="Avatar" />
+                  </label>
+                ) : (
+                  <DefaultAvatar>
+                    <ImgAvatar src={defaultAvatar} alt="Avatar" />
+                  </DefaultAvatar>
+                )}
+              </AvatarWrapper>
+              <FieldAdd
+                id="add-avatar"
+                name="avatar"
+                type="file"
+                accept="image/*, .png,.jpg, .gif"
+                onChange={e => {
+                  setAvatarURL(e.target.files[0]);
+                }}
+              />
+              <label htmlFor="add-avatar">
+                <AddIcon />
               </label>
-
-              <label>
-                Birthday
-                <LocalizationProvider dateAdapter={AdapterDayjs}>
-                  <DatePicker
-                    name="birthday"
-                    type="date"
-                    slotProps={{
-                      textField: {
-                        placeholder: userInfo.birthday || `${currentDate}`,
-                      },
-                    }}
-                    views={['year', 'month', 'day']}
-                    format="DD/MM/YYYY"
-                    closeOnSelect={true}
-                    disableFuture={true}
-                  />
-                </LocalizationProvider>
-              </label>
-              <div>
+            </div>
+            <h1>{userInfo.name}</h1>
+            <p>User</p>
+            <UserWrapper>
+              <UserInfo>
                 <label>
-                  Email
-                  <Field name="email" type="email" />
-                  <ErrorMessage name="email" component="span" />
+                  User Name
+                  <Field name="name" />
+                  <ErrorMessage name="name" component="span" />
                 </label>
-              </div>
-            </UserInfo>
 
-            <UserInfo>
-              <label>
-                Phone
-                <Field name="phone" type="tel" placeholder="+380XXXXXXXXX" />
-                <ErrorMessage name="phone" component="span" />
-              </label>
+                <label>
+                  Birthday
+                  <LocalizationProvider dateAdapter={AdapterDayjs}>
+                    <DatePicker
+                      name="birthday"
+                      type="date"
+                      slotProps={{
+                        textField: {
+                          placeholder: userInfo.birthday || `${currentDate}`,
+                        },
+                      }}
+                      views={['year', 'month', 'day']}
+                      format="YYYY/MM/DD"
+                      closeOnSelect={true}
+                      disableFuture={true}
+                      onChange={date => {
+                        if (!date) setFieldValue('birthday', '');
+                        setFieldValue('birthday', date);
+                        setBirthDate(date);
+                      }}
+                    />
+                  </LocalizationProvider>
+                </label>
+                <div>
+                  <label>
+                    Email
+                    <Field name="email" type="email" />
+                    <ErrorMessage name="email" component="span" />
+                  </label>
+                </div>
+              </UserInfo>
 
-              <label>
-                Skype
-                <Field
-                  name="skype"
-                  type="text"
-                  placeholder="Add a skype number"
-                />
-                <ErrorMessage name="skype" component="span" />
-              </label>
-            </UserInfo>
-          </UserWrapper>
+              <UserInfo>
+                <label>
+                  Phone
+                  <Field name="phone" type="tel" placeholder="+380XXXXXXXXX" />
+                  <ErrorMessage name="phone" component="span" />
+                </label>
 
-          <button type="submit">Save changes</button>
-        </WrapperForm>
-      </Form>
-    </Formik>
+                <label>
+                  Skype
+                  <Field
+                    name="skype"
+                    type="text"
+                    placeholder="Add a skype number"
+                  />
+                  <ErrorMessage name="skype" component="span" />
+                </label>
+              </UserInfo>
+            </UserWrapper>
+
+            <button type="submit">Save changes</button>
+          </Form>
+        )}
+      </Formik>
+    </WrapperForm>
   );
 };
 
