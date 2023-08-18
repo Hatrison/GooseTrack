91431eebab45a9a65975e18a1c815e1d90460a8c
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-18 12:22:28 +0300

    Improved validation

--- src/components/FeedbackForm/FeedbackFormSchema.js
+++ src/components/FeedbackForm/FeedbackFormSchema.js
@@ -1,6 +1,6 @@
 import * as Yup from 'yup';
 
 export const FeedbackFormSchema = Yup.object().shape({
-  rating: Yup.number().required('Required'),
-  text: Yup.string().required('Required'),
+  rating: Yup.number().min(1, 'Required field').required(),
+  text: Yup.string().required('Required field'),
 });
