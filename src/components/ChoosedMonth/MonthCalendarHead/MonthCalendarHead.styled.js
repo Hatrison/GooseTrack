00f83da26ddef350b8ea1e9cf604b27f48695f2f
SerhiiRud <carmagsales@gmail.com>
2023-08-18 22:29:17 +0300

    update

---
+++ src/components/ChoosedMonth/MonthCalendarHead/MonthCalendarHead.styled.js
@@ -0,0 +1,44 @@
+import styled from '@emotion/styled';
+
+export const StyledList = styled.ul`
+  display: flex;
+  justify-content: space-around;
+
+  padding: 14px 0px;
+
+  border: 1px solid rgba(220, 227, 229, 0.8);
+  border-radius: 8px;
+  margin-bottom: 14px;
+
+  background-color: ${props => props.theme.bcgTernary};
+
+  @media (min-width: 768px) {
+    margin-bottom: 18px;
+  }
+  @media (min-width: 1440px) {
+    margin-bottom: 15px;
+  }
+`;
+export const StyledItem = styled.li`
+  color: ${props =>
+    props.item === 'SAT' || props.item === 'SUN'
+      ? 'var(--main-blue-color);'
+      : props.theme.calendarHeadDate};
+
+  .full-name {
+    display: block;
+  }
+  .short-name {
+    display: none;
+  }
+
+  @media (max-width: 480px) {
+    .full-name {
+      display: none;
+    }
+
+    .short-name {
+      display: block;
+    }
+  }
+`;
