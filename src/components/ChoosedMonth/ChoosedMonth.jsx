8d0cc9c827610ce292d5b3a087d7102c5a428788
SerhiiRud <carmagsales@gmail.com>
2023-08-18 14:40:39 +0300

    update

---
+++ src/components/ChoosedMonth/ChoosedMonth.jsx
@@ -0,0 +1,29 @@
+import CalendarTable from './components/CalendarTable/CalendarTable';
+import MonthCalendarHead from './components/MonthCalendarHead/MonthCalendarHead';
+import { useSelector } from 'react-redux';
+import { selectDate } from 'redux/date/selectors';
+import { selectTasks } from 'redux/tasks/selectors';
+import { endOfMonth, startOfMonth, isWithinInterval } from 'date-fns';
+import { Wrapper } from './ChoosedMonth.styled';
+
+const ChoosedMonth = () => {
+  const currentDate = useSelector(selectDate);
+  const tasks = useSelector(selectTasks);
+
+  const startMonth = startOfMonth(new Date(currentDate));
+  const endMonth = endOfMonth(new Date(currentDate));
+
+  const filteredDates = tasks.filter(task => {
+    const date = new Date(task.date);
+    return isWithinInterval(date, { start: startMonth, end: endMonth });
+  });
+
+  return (
+    <Wrapper>
+      <MonthCalendarHead />
+      <CalendarTable tasks={filteredDates} currentDate={currentDate} />
+    </Wrapper>
+  );
+};
+
+export default ChoosedMonth;
