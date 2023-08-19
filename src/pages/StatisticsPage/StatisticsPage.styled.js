0db5b283a00f1c0ea7339e9413a7ffc77b618c74
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-20 00:58:53 +0300

    Fixed styles for flex position in statistics

--- src/pages/StatisticsPage/StatisticsPage.styled.js
+++ src/pages/StatisticsPage/StatisticsPage.styled.js
@@ -5,15 +5,18 @@ export const StatisticsWrapper = styled.div`
   display: flex;
   flex-basis: 100%;
   flex-direction: column;
-  justify-content: center;
+  justify-content: flex-start;
   align-items: center;
   border-radius: 16px;
   background-color: ${({ theme }) => theme.mainBackgroundColor};
   width: 100%;
   padding: 0 14px;
+  padding-top: 34px;
 
   @media screen and (min-width: 768px) {
     padding: 0 32px;
+    justify-content: center;
+    padding-top: 0;
   }
 
   @media screen and (min-width: 1280px) {
