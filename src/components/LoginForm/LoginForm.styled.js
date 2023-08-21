d58a37a887b3ab25bb450ef537b4ab9334021752
Vadim Ulanovskiy <vadimulanovskiy@gmail.com>
2023-08-21 17:10:07 +0300

    Added styles for auth navigate

--- src/components/LoginForm/LoginForm.styled.js
+++ src/components/LoginForm/LoginForm.styled.js
@@ -15,33 +15,6 @@ export const FormContainer = styled.div`
   align-items: center;
 
   padding: 20px;
-
-  & > a {
-    position: relative;
-    display: block;
-    font-family: 'InterSemiBolt', sans-serif;
-    font-style: normal;
-    font-weight: 600;
-    font-size: 12px;
-    line-height: 14px;
-
-    color: ${props => props.theme.accentColor};
-
-    &:before {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-
-      content: '';
-      width: 100%;
-      height: 1px;
-      background-color: ${props => props.theme.accentColor};
-    }
-    @media (min-width: 768px) {
-      font-size: 18px;
-      line-height: 24px;
-    }
-  }
 `;
 
 export const Form = styled(FormikForm)`
