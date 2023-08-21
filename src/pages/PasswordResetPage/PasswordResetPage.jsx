8b6a53879978f38c9ecc9a984189e51cac1203d7
vasylenkodi <vasylenko1daniil@gmail.com>
2023-08-21 14:42:57 +0200

    додає функцію ресету пароля

--- src/pages/PasswordResetPage/PasswordResetPage.jsx
+++ src/pages/PasswordResetPage/PasswordResetPage.jsx
@@ -24,73 +24,74 @@ import { loginFormSchema } from '../../components/LoginForm/loginFormSchema';
 import GooseRocket from 'images/png/goose-rocket-login/goose-login.png';
 import GooseRocketRetina from 'images/png/goose-rocket-login/goose-login@2x.png';
 
+const initialState = { email: '' };
+
 const PasswordResetPage = () => {
   const dispatch = useDispatch();
 
   return (
-    <FormContainer>
-      <Formik
-        initialValues={{ email: '' }}
-        onSubmit={(values, actions) => {
-          console.log('submited');
-          dispatch(
-            resetPassword({ email: values.email })
-          )
-            .then(data => {
-              if (data.payload.name) {
-              }
-              if (data.error) {
-                throw new Error(`The email is not found`);
-              }
-              actions.resetForm();
-            })
-            .catch(error => {
-              toast.error(error.message);
-            });
-        }}
-        validationSchema={loginFormSchema}
-      >
-        {({ errors, touched }) => (
-          <Form>
-            <FormTitle>Reset password</FormTitle>
-            <FormField
-              htmlFor="email"
-              style={{
-                color:
-                  (touched.email && errors.email && '#E74A3B') ||
-                  (touched.email && !errors.email && '#3CBC81'),
-              }}
-            >
-              Email
-            </FormField>
-            <IconContainer>
-              <Field
-                type="email"
-                name="email"
-                placeholder="Your email"
+    <>
+      <FormContainer>
+        <Formik
+          initialValues={initialState}
+          onSubmit={(values, actions) => {
+            dispatch(resetPassword({ email: values.email }))
+              .then(data => {
+                if (data.payload.name) {
+                }
+                if (data.error) {
+                  throw new Error(`The email is not found`);
+                }
+                actions.resetForm();
+              })
+              .catch(error => {
+                toast.error(error.message);
+              });
+          }}
+          validationSchema={loginFormSchema}
+        >
+          {({ errors, touched }) => (
+            <Form>
+              <FormTitle>Reset password</FormTitle>
+              <FormField
+                htmlFor="email"
                 style={{
-                  borderColor:
+                  color:
                     (touched.email && errors.email && '#E74A3B') ||
                     (touched.email && !errors.email && '#3CBC81'),
                 }}
-              />
-              {touched.email && errors.email && <ErrorIcon />}
-              {touched.email && !errors.email && <CorrectIcon />}
-            </IconContainer>
+              >
+                Email
+              </FormField>
+              <IconContainer>
+                <Field
+                  type="email"
+                  name="email"
+                  placeholder="Your email"
+                  style={{
+                    borderColor:
+                      (touched.email && errors.email && '#E74A3B') ||
+                      (touched.email && !errors.email && '#3CBC81'),
+                  }}
+                />
+                {touched.email && errors.email && <ErrorIcon />}
+                {touched.email && !errors.email && <CorrectIcon />}
+              </IconContainer>
 
-            <SubmitBtn type="submit">
-              Reset password
-              <ButtonIcon />
-            </SubmitBtn>
-          </Form>
-        )}
-      </Formik>
-      <GooseIMG
-        srcset={`${GooseRocket} 1x, ${GooseRocketRetina} 2x`}
-        src={`${GooseRocket}`}
-        alt="goose"
-      />
-    </FormContainer>
+              <SubmitBtn type="submit">
+                Reset password
+                <ButtonIcon />
+              </SubmitBtn>
+            </Form>
+          )}
+        </Formik>
+        <GooseIMG
+          srcset={`${GooseRocket} 1x, ${GooseRocketRetina} 2x`}
+          src={`${GooseRocket}`}
+          alt="goose"
+        />
+      </FormContainer>
+    </>
   );
 };
 
